dfa34cfc0d750e1a5235c5f654053d54590f65d5
um8389 <um8389@kaist.ac.kr>
2023-07-26 17:16:14 +0900

    suckit

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,14 +1,35 @@
 import { Link } from "react-router-dom";
+import profileImage from '../images/image1.jpeg'
+import { HiUser } from "react-icons/hi";
 
 const data = ["AAA", "BBB", "CCC", "DDD","AAA", "BBB", "CCC", "DDD","AAA", "BBB", "CCC", "DDD","AAA", "BBB", "CCC", "DDD",];
 
 function Friend() {
   return (
-    <div style={{ overflowY: "auto", height: "650px" }}>
+    <div style={{ overflowY: "auto", height: "650px",}}>
       {data.map((id) => (
-        <div key={id}>
+        <div key={id} style={{paddingTop:'2px',height:'40px',border:'2px solid #bbbbbb',marginLeft:'30px',textAlign:'left', marginBottom:'20px', marginRight:'30px',paddingLeft:'8px', borderRadius:10}}>
           <Link to={`/${id}`}>
-            <h3 style={{fontSize:15,color:'#333333'}}>{id}</h3>
+            <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+              <div style={{width:"27px", height:"27px", border:'2px solid #1188bb', borderRadius:10,display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <img
+                  src={profileImage} // 이미지 파일의 경로
+                  alt="Profile"
+                  style={{
+                    width: "25px", // 이미지 크기 조절
+                    height: "25px",
+                    borderRadius: 10, // 원형으로 만들기 위한 속성
+                    objectFit: "cover", // 이미지가 원형으로 잘릴 경우를 대비하여 가리기 위한 속성
+                  }}
+                />
+              </div>
+              
+              <h3 style={{marginLeft:'15px',fontSize:12,color:'#444444'}}>{id}</h3>
+              
+              <HiUser style={{fontSize:20,paddingLeft:'115px', color:'#444444'}}/>
+              
+            </div>
+            
           </Link>
           <br />
         </div>
--- src/components/content/PostList.js
+++ src/components/content/PostList.js
@@ -1,6 +1,6 @@
 import { useParams, Link } from "react-router-dom";
-
-const data = ["1", "2", "3", "4"];
+import {RxDotFilled} from "react-icons/rx";
+const data = ["인생은 아름다워", "Soul(filmed by Pixar)", "Eternal Sunshine", "Old Boy(Chanwook Park)","조제, 호랑이, 그리고 물고기들", "LaLa Land", "Monster(Junho Bong)", "Border Line","노인을 위한 나라는 없다", "시카리오: 암살자들의 도시", "펄프 픽션(쿠엔틴 타란티노)", "아이리쉬맨(마틴 스코세이지)","파벨만스(스티븐 스필버그)", "봄날은 간다(유지태, 이영애)", "Matrix(키아누 리브스)", "500 days of summer","Her", "Big Fish", "Terminal", "Forrest Gump", "GodFather", "Zurasic world", "극한직업", "기생충", "Moonlight", "NomadLand"];
 
 //useEffect를 통해 categoryName을 query로 보내면
 // 일치하는 것들의 record를 가져온다.
@@ -8,21 +8,47 @@ const data = ["1", "2", "3", "4"];
 function PostList() {
   const { categoryName } = useParams();
   return (
-    <div>
-      {categoryName} 게시판 입니다.
-      <div>
-        {data.map((id) => (
-          <div key={id}>
-            <Link to={`/${categoryName}/${id}`}>
-              <h3>
-                {categoryName}의 {id} 게시물 클릭하기
-              </h3>
-            </Link>
-            <br />
+    <div style={{flexDirection:'row', display:'flex',}}>
+      <div style={{width:'370px', height:'650px', marginRight:'10px',}}>
+            <h4 style={{borderRadius:5,marginLeft:'5px',border:'1px solid black', width:'125px', paddingLeft:'10px',height:'25px', display:'flex',flexDirection:'row', alignItems:'center',marginBottom:'27px'}}>
+              {categoryName}의 이야기들
+            </h4>
+            
+            <div style={{ overflowY: "auto", height: "610px"}}>
+              {data.map((id) => (
+                <div key={id}>
+                  <Link to={`/${categoryName}/${id}`}>
+                    <div style={{color:'#333333',fontSize:13, borderTop:'1px solid lightgray', borderBottom:'1px solid lightgray', width:'370px'}}>
+                      <RxDotFilled style={{fontSize:9,paddingRight:'5px'}}/>{categoryName}: {id}
+                    </div>
+                  </Link>
+                  <br />
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+          <div style={{width:'1px', height:'680px', backgroundColor:'black', marginRight:'20px'}}>
+          </div>
+          <div style={{width:'370px', height:'650px',}}>
+            <h4 style={{borderRadius:5,marginLeft:'5px',border:'1px solid black', width:'125px', paddingLeft:'10px',height:'25px', display:'flex',flexDirection:'row', alignItems:'center',marginBottom:'27px'}}>
+              모두의 이야기들
+            </h4>
+            
+            <div style={{ overflowY: "auto", height: "610px"}}>
+              {data.map((id) => (
+                <div key={id}>
+                  <Link to={`/${categoryName}/${id}`}>
+                    <div style={{color:'#333333',fontSize:13, borderTop:'1px solid lightgray', borderBottom:'1px solid lightgray', width:'370px'}}>
+                      <RxDotFilled style={{fontSize:9, paddingRight:'5px'}}/> {id}
+                    </div>
+                  </Link>
+                  <br />
+                </div>
+              ))}
+            </div>
+          </div>
     </div>
+    
   );
 }
 
